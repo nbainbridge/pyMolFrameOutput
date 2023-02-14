4cc3510db4e04b0e8ac7e5de6cf5415d1dc8a688
Nick Bainbridge <105810278+nbainbridge@users.noreply.github.com>
2023-02-14 09:16:52 -0500

    Add files via upload

--- FrameSave.py
+++ FrameSave.py
@@ -1,11 +1,34 @@
 from pymol import cmd, stored
+"""
+ ___Author___  =  Nicholas Bainbridge nbainbr@uwo.ca
+ ___Date___    =  02/09/2023
+ ___Version___ =  1.0
 
-def save(name, start, stop, interval):
-	name = string(name)
-	start = int(start)
-	stop = int(stop)
-	interval = int(interval)
+Exports a series of individual pdb files from a given number of frames given the starting frame, last frame, and an interval
+
+Example: to output every 500th frame starting from frame 0 to 2000 as filename test:
+	framesave test, 0, 2000, 500
 	
-	cmd.save(name, "all", start, "pdb")
+Output: 
+	test0.pdb, test500.pdb, test1000.pdb, test1500.pdb, test2000.pdb
+
+
+"""
+def framesave(filename, start, stop, interval, selection='(all)', state=0, format='pdb', ref='',ref_state=-1, quiet=1, partial=0):
 	
-cmd.extend("save", save)
+	#	Getting First Frame
+	x = int(start)
+	
+	# Iterate over every frame to export, stopping at last frame
+	while int(x) <= int(stop):
+	
+	# Setting new filename to registered file name + frame number + .pdb format
+		newfilename = filename + str(x)+'.pdb'
+	
+	#Save file
+		cmd.save(newfilename, selection, x,format)
+	
+	#Iterator
+		x = x + int(interval)
+		
+cmd.extend("framesave", framesave)
